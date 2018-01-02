9e096480e051e4d58d0936398202a156fe62d424
Dave Cottlehuber <dch@skunkwerks.at>
2018-01-02 16:21:58 +0000

    mix it up
    - credo
    - distillery
    - basic rel/config.exs

---
+++ rel/config.exs
@@ -0,0 +1,27 @@
+Path.join(["rel", "plugins", "*.exs"])
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+    default_release: :default,
+    default_environment: Mix.env()
+
+environment :dev do
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"nomnomnom"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"nomnomnom"
+end
+
+release :katie do
+  set version: current_version(:katie)
+  set applications: [
+    :runtime_tools
+  ]
+end
+
