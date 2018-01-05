4a9db2d7063ea080524b076b748d08d2cd3dbf5b
Dave Cottlehuber <dch@skunkwerks.at>
2018-01-05 20:34:22 +0000

    katie: start kterl in supervision tree

--- lib/katie/application.ex
+++ lib/katie/application.ex
@@ -1,19 +1,13 @@
 defmodule Katie.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Katie.Worker.start_link(arg)
-      # {Katie.Worker, arg},
+      %{id: :kterl,
+      start: {:kterl, :start_link, [[host: '10.241.0.0']]}}
     ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Katie.Supervisor]
     Supervisor.start_link(children, opts)
   end
